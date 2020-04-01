3c3ba680b6a26e6cda0ce360dc63ed94efb7342e
root <root@devx-python001.ziyotek.local>
2020-04-01 19:35:54 -0400

    added new changes

---
+++ conditionals.py
@@ -0,0 +1,8 @@
+likes_pineapple_on_pizza = "Absolutely!"
+
+if(likes_pineapple_on_pizza == "Absolutely!"):
+    print("You are weird!")
+elif(likes_pineapple_on_pizza == "Sort of"):
+    print("Iguess you're okay")
+else:
+    print("you are completely normal")
--- game.py
+++ game.py
@@ -1,3 +1,11 @@
 # Author: Ryan V. Murdock
 # Contact: email
 #information about the code: A fun little python game
+
+name = input("What is your name?")
+age = int(input("What is your age?"))
+your_age_in_hundred_years = age+100
+
+
+print("Hi, " + name + "!")
+print("Your age is " + str(age) + " but in 100 years, you are going to be " + str(your_age_in_hundred_years))
